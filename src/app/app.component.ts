18b32b7525d14711e82c53109f485c0d01be8e3b
The Anh <anhtheit2002@gmail.com>
2024-03-12 21:43:43 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    OnChanges,
+    OnInit,
+    QueryList,
+    ViewChild,
+    ViewChildren,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { ToggleComponent } from './components/toggle/toggle.component';
@@ -6,12 +16,37 @@ import { ToggleComponent } from './components/toggle/toggle.component';
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, FormsModule, ToggleComponent],
+    imports: [RouterOutlet, FormsModule, ToggleComponent, CommonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+    @ViewChild('inputRef', { static: true }) inputRef!: ElementRef<HTMLInputElement>;
+    @ViewChild('toggleRef2') toggleRefAlias!: ToggleComponent;
+
+    @ViewChildren('toggleRef') toggleRefs!: QueryList<ToggleComponent>;
+
     title = 'angular-learn';
     name = 'world';
-    isChecked = false;
+
+    isCheckedBtn1 = false;
+    isCheckedBtn2 = false;
+
+    count = 0;
+    isCheckedAll = false;
+    isShowLast = true;
+
+    ngOnInit(): void {
+        this.inputRef.nativeElement.focus();
+    }
+
+    ngAfterViewInit(): void {
+        this.toggleRefs.changes.subscribe(() => {
+            setTimeout(() => (this.count = this.toggleRefs.length), 0);
+        });
+    }
+
+    handleToggle() {
+        this.toggleRefAlias.toggle();
+    }
 }
--- src/app/components/toggle/toggle.component.ts
+++ src/app/components/toggle/toggle.component.ts
@@ -10,4 +10,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class ToggleComponent {
     @Input() checked = false;
     @Output() checkedChange = new EventEmitter();
+
+    toggle() {
+        this.checkedChange.emit(!this.checked);
+    }
 }
