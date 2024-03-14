70d16b5eec740be250ca8bf13b2251bb75a1eebd
The Anh <anhtheit2002@gmail.com>
2024-03-14 22:35:31 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, CardComponent],
+    imports: [RouterOutlet, NgTemplateOutlet, CardComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent {
     title = 'angular-learn';
+    myContext = { $implicit: 'Foo', myText: 'Hello everyone' };
 }
