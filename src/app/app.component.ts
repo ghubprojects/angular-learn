1aac07f5bbad2b22f5818cd754f694f2b4fe9799
The Anh <anhtheit2002@gmail.com>
2024-03-22 00:36:17 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+
+const observable = new Observable((observer) => {
+    const id = setInterval(() => {
+        observer.next('Hello Rxjs');
+    }, 1000);
+
+    return () => clearInterval(id);
+});
+
+const subcription = observable.subscribe({
+    next: (value) => console.log(value),
+    error: (error) => console.error(error),
+    complete: () => console.log('completed'),
+});
+
+subcription.add(observable.subscribe(console.log));
+
+setTimeout(() => subcription.unsubscribe(), 5000);
 
 @Component({
     selector: 'app-root',
