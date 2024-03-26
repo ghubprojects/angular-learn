a8d7879ba66bd824bb9159d5c2662e354da30fa4
The Anh <anhtheit2002@gmail.com>
2024-03-26 18:23:34 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject, finalize, of, ReplaySubject, Subject, switchMap, timer } from 'rxjs';
+
+const createObserver = (observer: any) => ({
+    next: (value: any) => console.log(observer, value),
+    error: (error: any) => console.error(observer, error),
+    complete: () => console.log('completed'),
+});
+
+/* Subject */
+const loadingSubject = new Subject();
+function getUsers() {
+    loadingSubject.next(true);
+    return timer(3000).pipe(
+        switchMap(() => of('users')),
+        finalize(() => loadingSubject.next(false)),
+    );
+}
+
+loadingSubject.subscribe(createObserver('Component'));
+getUsers().subscribe();
+
+/* Behavior Subject */
+const behaviorSubject = new BehaviorSubject('hello');
+behaviorSubject.subscribe(createObserver('Component A'));
+behaviorSubject.next('world');
+behaviorSubject.subscribe(createObserver('Component B'));
+console.log(behaviorSubject.value);
+
+/* Replay Subject */
+const replaySubject = new ReplaySubject(2);
+replaySubject.subscribe(createObserver('Component A'));
+replaySubject.next('hello');
+replaySubject.next('world');
+replaySubject.next('rxjs');
+replaySubject.subscribe(createObserver('Component B'));
 
 @Component({
     selector: 'app-root',
